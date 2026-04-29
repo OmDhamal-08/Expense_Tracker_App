ba422900876f4520f0fa69f14543c02c5cb02a97
OmDhamal-08 <omdhamal77@gmail.com>
2026-04-29 23:15:42 +0530

    Auto create superuser from env vars

--- build.sh
+++ build.sh
@@ -10,3 +10,8 @@ python manage.py migrate
 
 # Seed predefined categories + payment methods (safe to run multiple times)
 python manage.py seed_defaults
+
+# Automatically create a superuser if environment variables are set
+if [[ -n "${DJANGO_SUPERUSER_USERNAME}" && -n "${DJANGO_SUPERUSER_PASSWORD}" && -n "${DJANGO_SUPERUSER_EMAIL}" ]]; then
+  python manage.py createsuperuser --noinput || true
+fi
